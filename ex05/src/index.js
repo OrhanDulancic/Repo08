bba713a975f176f424694da498c32aa498ac3742
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-04 15:36:10 +0200

    Undefined

---
+++ ex05/src/index.js
@@ -0,0 +1,24 @@
+var sum =0;  
+
+function addThree() {
+  sum = sum + 3;
+
+  console.log("addThree: " + sum);
+  
+}
+
+function addFive() {
+  sum = sum + 5;
+  console.log("addFive: "+ sum);
+  
+}
+
+console.log(addThree());
+console.log(addFive());
+
+
+
+module.exports = {
+  addThree,
+  addFive,
+};
